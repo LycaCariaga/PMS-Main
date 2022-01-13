5ba4ef6917ee82efe4b0d1d6bad4f6d9204d4fcb
VinnieKiro89 <vinnieultimate@gmail.com>
2022-01-13 12:58:08 +0800

    test Patient table migration

---
+++ database/migrations/2022_01_13_125247_create_patients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+
+            // $table->string('name');// this stands as username
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->string('middlename');
+            $table->date('birthday');
+            $table->string('department');
+            $table->string('gender');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
